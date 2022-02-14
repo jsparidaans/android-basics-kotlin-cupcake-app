32bf0bd92ed0b4e214fc4284364c8a41162feac2
Joey <jsparidaans@gmail.com>
2022-02-14 13:56:00 +0100

    formatting

--- app/src/main/java/com/example/cupcake/FlavorFragment.kt
+++ app/src/main/java/com/example/cupcake/FlavorFragment.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -63,7 +62,7 @@ class FlavorFragment : Fragment() {
         findNavController().navigate(R.id.action_flavorFragment_to_pickupFragment)
     }
 
-    fun cancelOrder(){
+    fun cancelOrder() {
         sharedViewModel.resetOrder()
         findNavController().navigate(R.id.action_flavorFragment_to_startFragment)
     }
--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -19,7 +19,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -63,10 +62,11 @@ class PickupFragment : Fragment() {
         findNavController().navigate(R.id.action_pickupFragment_to_summaryFragment)
     }
 
-    fun cancelOrder(){
+    fun cancelOrder() {
         sharedViewModel.resetOrder()
         findNavController().navigate(R.id.action_pickupFragment_to_startFragment)
     }
+
     /**
      * This fragment lifecycle method is called when the view hierarchy associated with the fragment
      * is being removed. As a result, clear out the binding object.
